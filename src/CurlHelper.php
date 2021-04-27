8d78c81eafe79696608b570dde775e93e2492cf9
zhengze <zhengze@beihai365.com>
2021-04-27 09:09:24 +0800

    优化curl模块

--- src/CurlHelper.php
+++ src/CurlHelper.php
@@ -8,7 +8,6 @@
 namespace Helper;
 
 
-
 /**
  * Class CurlFunc
  * @package Helper
@@ -17,80 +16,87 @@ class CurlHelper
 {
 
     /**
- * @param string $url post请求地址
- * @param array $params
- * @return mixed
- */
-public static function curlPost($url, array $params = array(), $token = '')
-{
-    $data_string = json_encode($params);
-    $ch = curl_init();
+     * @param string $url post请求地址
+     * @param array $params
+     *
+     * @return mixed
+     */
+    public static function curlPost($url, array $params = [], $token = '')
+    {
+        $data_string = json_encode($params);
+        $ch = curl_init();
 //   设置url
-    curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_URL, $url);
 //    移除返回的头信息
-    curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
 //    信息以字符串返回
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 //    在尝试连接时等待的秒数
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
 //    请求类型,true为post
-    curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
 //    证书校验
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
 //    传送数据
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 //    设置 HTTP 头字段的数组
-    $header = array(
-        'Content-Type: application/json'
-    );
-    if (!empty($token)) {
-        $header['token'] = "token:$token";
+        $header = [
+            'Content-Type: application/json',
+        ];
+        if (!empty($token)) {
+            $header['token'] = "token:$token";
+        }
+        curl_setopt(
+            $ch, CURLOPT_HTTPHEADER, $header
+        );
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return ($data);
     }
-    curl_setopt(
-        $ch, CURLOPT_HTTPHEADER, $header
-    );
-    $data = curl_exec($ch);
-    curl_close($ch);
-    return ($data);
-}
 
-public static function curlPostRaw($url, $rawData)
-{
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $rawData);
-    curl_setopt(
-        $ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: text'
-        )
-    );
-    $data = curl_exec($ch);
-    curl_close($ch);
-    return $data;
-}
+    public static function curlPostRaw($url, $rawData)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_POST, 1);
+//    证书校验
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $rawData);
+        curl_setopt(
+            $ch, CURLOPT_HTTPHEADER, [
+                'Content-Type: text',
+            ]
+        );
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
 
-/**
- * @param string $url get请求地址
- * @param int $httpCode 返回状态码
- * @return mixed
- */
-public static function curlGet($url, &$httpCode = 0)
-{
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    /**
+     * @param string $url get请求地址
+     * @param int $httpCode 返回状态码
+     *
+     * @return mixed
+     */
+    public function curlGet($url, array $data = [], &$httpCode = 0)
+    {
+        $ch = curl_init();
+        if (!empty($data)) {
+            $data = is_array($data) ? http_build_query($data) : trim($data, '&');
+            $url = rtrim($url, '&') . (strpos($url, '?') === FALSE ? '?' : '&') . $data;
+        }
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-    //不做证书校验,部署在linux环境下请改为true
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-    $file_contents = curl_exec($ch);
-    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
-    return $file_contents;
-}
+//    证书校验
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+        $file_contents = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        return $file_contents;
+    }
 }
